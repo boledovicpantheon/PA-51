a8f38020a29a9d928ebab95d16b75657a7d3e84e
boledovicpantheon <maros.boledovic@pantheon.tech>
2022-09-08 14:34:05 +0200

    added github actions

--- bash
+++
@@ -1,6 +0,0 @@
-docker login docker.registry.com:5000 -u user -p password
-docker pull ubuntu:latest
-docker tag ubuntu:latest docker.registry.com:5000/ubuntu:v1
-docker push docker.registry.com:5000/ubuntu:v1
-kubectl create -f /vagrant/deployment/test.yaml
-sleep 5 && kubectl describe pod test
---
+++ bash.sh
@@ -0,0 +1,13 @@
+docker login docker.registry.com:5000 -u user -p password
+
+docker pull ubuntu:latest
+docker tag ubuntu:latest docker.registry.com:5000/ubuntu:v1
+docker push docker.registry.com:5000/ubuntu:v1
+
+docker pull nginx:latest
+docker tag nginx:latest docker.registry.com:5000/nginx:v1
+docker push docker.registry.com:5000/nginx:v1
+
+docker image rm docker.registry.com:5000/nginx:v1 docker.registry.com:5000/ubuntu:v1
+
+kubectl create -f /vagrant/deployment/nginx-deployment.yaml 
